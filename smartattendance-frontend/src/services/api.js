1e4667a389579c4238cac34441118819a2a4453d
Tamilselvi <smtp100626@gmail.com>
2026-07-02 10:11:03 +0530

    Updated backend URL

--- smartattendance-frontend/src/services/api.js
+++ smartattendance-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL:  "https://smart-attendance-backend-2zm3.onrender.com",
 });
 
 export default API;
